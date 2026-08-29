5d366151473f4f99aa7caeb064972a5ba54e714e
Chathruknan Sathvathu <vvruchendran141594@gmail.com>
2026-08-29 11:38:19 +0530

    upd one

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -10,18 +10,18 @@ authRoute.post("/login",(req:Request,res:Response,next:NextFunction)=>{
 //new user registration.
 authRoute.post("/register",async(req:Request,res:Response,next:NextFunction)=>{
     // console.log("request triggered",req.body);
-    const inserValues=(values:object)=>{
+    // const inserValues=(values:object)=>{
 
-    }
-    const db = getDb();
-    await db.query(registerTableQuery);
-    const [regis] = await db.query("show tables like 'register'");
-    if(regis.length === 0){
-        await db.query(registerTableQuery);
-    }else{
-        console.log("Table already exists");
-    }
-    const {username,email,password} = req.body;
+    // }
+    // const db = getDb();
+    // await db.query(registerTableQuery);
+    // const [regis] = await db.query("show tables like 'register'");
+    // if(regis.length === 0){
+    //     await db.query(registerTableQuery);
+    // }else{
+    //     console.log("Table already exists");
+    // }
+    // const {username,email,password} = req.body;
     res.send("User registered successfully");
     
 })
